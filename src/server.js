ec3db7f68539f591a31d9a25fd5a6c64b1ff915c
Lucian Maran <lucian.maran@outlook.com>
2024-05-29 13:23:57 +0300

    Improve the build action

--- src/server.js
+++ src/server.js
@@ -1,17 +1,17 @@
-//const express = require('express')
 import express from 'express';
 
-const app = express()
-const port = 1420
+const app = express();
+const port = 1420;
 
 app.get('/', (req, res) => {
-  res.send('Hello Docupedia!')
+    res.send('Hello world!')
 })
 
+ // Health check for HAProxy (ex: docupedia-blue-production)
 app.get("/check", (req, res) => {
-  res.send("docupedia-" + (process.env.DEPLOYMENT_SLOT || "noslot") + "-" + process.env.NODE_ENV);
+    res.send(`docupedia-${process.env.DEPLOYMENT_SLOT || "noslot"}-${process.env.NODE_ENV || "noenv"}`);
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Listening on port ${port}`)
 })
