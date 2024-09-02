b8cd92bc416d451074b00fd3206b0df90508e066
Lucian M <lucian.maran@gmail.com>
2024-09-02 19:29:27 +0300

    Split into server.js and app.js

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,18 @@
 import express from "express";
+import config from "./config/config.js";
 import * as testController from "./controllers/test.controller.js";
 
 const router = express.Router();
+
+router.get("/", (req, res) => {
+    res.send(`Hello world! Var1=${config.var1}, Secret1=${config.secret1}`);
+});
+
+// Health check for HAProxy (ex: docupedia-blue-production)
+router.get("/check", (req, res) => {
+    res.send(`docupedia-${config.deployment_slot}-${config.env}`);
+});
+
 router.get("/test", testController.getAll);
 
 export default router;
--- src/server.js
+++ src/server.js
@@ -1,20 +1,6 @@
 import browserSync from "browser-sync";
-import express from "express";
+import app from "./app.js";
 import config from "./config/config.js";
-import routes from "./routes.js";
-
-const app = express();
-
-app.get("/", (req, res) => {
-    res.send(`Hello world! Var1=${config.var1}, Secret1=${config.secret1}`);
-});
-
-// Health check for HAProxy (ex: docupedia-blue-production)
-app.get("/check", (req, res) => {
-    res.send(`docupedia-${config.deployment_slot}-${config.env}`);
-});
-
-app.use("/", routes);
 
 app.listen(config.port, listening);
 
@@ -24,11 +10,9 @@ function listening() {
     // https://github.com/voorhoede/front-end-tooling-recipes/blob/master/express-with-nodemon-browsersync/index.js
     // https://ponyfoo.com/articles/a-browsersync-primer#inside-a-node-application
     if (`${config.env}` === "development") {
-        // const browserSync = require("browser-sync");
-
         browserSync({
             files: ["./web/views/**/*.js", "./**/*.hbs"],
-            online: true, // to have also an external url as 192.168.1.17:1417 for testing on mobile
+            online: true, // to have also an external url as 192.168.1.17:1421 for testing on mobile
             open: false,
             port: config.port,
             proxy: "localhost:" + config.port,
