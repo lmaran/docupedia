99281c96cafebdc676636e7bbe35f27615da2d11
Lucian M <lucian.maran@gmail.com>
2024-09-04 10:19:42 +0300

    Add Handlebars

--- src/app.js
+++ src/app.js
@@ -1,7 +1,21 @@
 import express from "express";
+import { engine } from "express-handlebars";
+import handlebarHelpers from "./helpers/handlebar.helper.js";
 import routes from "./routes.js";
 
 const app = express();
+
+// view engine setup
+app.engine(
+    ".hbs",
+    engine({
+        extname: ".hbs",
+        helpers: handlebarHelpers,
+    }),
+);
+app.set("view engine", ".hbs");
+app.set("views", "./src/views");
+
 app.use("/", routes);
 
 export default app;
